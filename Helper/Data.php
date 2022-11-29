1eaee0458d59f600781dad135f7e7c582f37305b
Gus deMayo <gus.demayo@gmail.com>
2022-11-29 10:51:15 -0600

    fixes typos in system.xml & encrypts password field

--- Helper/Data.php
+++ Helper/Data.php
@@ -2,16 +2,31 @@
 
 namespace Fruitcake\EmailAdvancedConfig\Helper;
 
-
 use Fruitcake\EmailAdvancedConfig\Model\Config\Source\SmtpTransportType;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Store\Model\ScopeInterface;
-
+use Magento\Framework\Encryption\EncryptorInterface;
 
 class Data extends AbstractHelper
 {
     const CONFIG_PATH = 'fruitcake_email_advanced/';
+
+    /**
+     * @var EncryptorInterface
+     */
+    private $encryptor;
+
+    /**
+     * @param Context $context
+     */
+    public function __construct(
+        Context $context,
+        EncryptorInterface $encryptor
+    ) {
+        parent::__construct($context);
+        $this->encryptor = $encryptor;
+    }
 
     public function isEnabled(): bool
     {
@@ -25,6 +40,10 @@ class Data extends AbstractHelper
      */
     public function getConfig($code = '')
     {
-        return $this->scopeConfig->getValue(self::CONFIG_PATH . $code, ScopeInterface::SCOPE_STORE, null);
+        $value = $this->scopeConfig->getValue(self::CONFIG_PATH . $code, ScopeInterface::SCOPE_STORE, null);
+
+        return ($code === 'smtp/password')
+            ? $this->encryptor->decrypt($value)
+            : $value;
     }
 }
