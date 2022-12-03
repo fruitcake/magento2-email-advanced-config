b75ad912e3b7b8f846f7230765a8406ceb8a485c
Gus deMayo <gus.demayo@gmail.com>
2022-12-02 18:56:48 -0600

    configuration typo update & begin encrypted password - encrypts existing passwords if present

---
+++ Setup/Module/UpgradeData.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Fruitcake\EmailAdvancedConfig\Setup\Module;
+
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Config\Storage\WriterInterface;
+use Magento\Framework\Encryption\EncryptorInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var WriterInterface
+     */
+    private $configWriter;
+
+    /**
+     * @var EncryptorInterface
+     */
+    private $encryptor;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param WriterInterface $configWriter
+     * @param EncryptorInterface $encryptor
+     */
+    public function __construct
+    (
+        ScopeConfigInterface $scopeConfig,
+        WriterInterface $configWriter,
+        EncryptorInterface $encryptor
+    )
+    {
+        $this->scopeConfig = $scopeConfig;
+        $this->configWriter = $configWriter;
+        $this->encryptor = $encryptor;
+    }
+
+    /**
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function upgrade
+    (
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+    )
+    {
+        if (version_compare($context->getVersion(), '1.0.1') > 0) {
+            $passwordPath = 'fruitcake_email_advanced/smtp/password';
+            $password = $this->scopeConfig->getValue($passwordPath, ScopeInterface::SCOPE_STORE);
+
+            if ($password && !$this->encryptor->decrypt($password)) {
+                $this->configWriter->save($passwordPath, $this->encryptor->encrypt($password));
+            }
+        }
+    }
+}
